a0a44db74929114a2d65743d0352fe921ee12ae9
Yaroslav Nikonorov <yaroslav.n@quali.com>
2019-04-09 12:28:48 +0300

    Added extracting logger to command logging

--- cloudshell/logging/utils/decorators.py
+++ cloudshell/logging/utils/decorators.py
@@ -1,20 +1,35 @@
+import inspect
+import logging
 from functools import wraps
 
 
 def command_logging(func):
     @wraps(func)
-    def wrapped(self, *args, **kwargs):
+    def wrapped(*args, **kwargs):
         func_name = func.__name__
 
-        self._logger.debu('Start command "{}"'.format(func_name))
+        # extracting logger
+        logger = None
+        if args:
+            logger = getattr(args[0], 'logger', None) or getattr(args[0], '_logger', None)
+            if not logger:
+                for var in list(args) + list(kwargs.values()):
+                    if isinstance(var, logging.Logger):
+                        logger = var
+                        break
+        if not logger:
+            raise Exception('Logger instance is not defined.')
+
+        logger.debug('Start command "{}"'.format(func_name))
         finishing_msg = 'Command "{}" finished {}'
         try:
-            result = func(self, *args, **kwargs)
+
+            result = func(*args, **kwargs)
         except Exception:
-            self._logger.info(finishing_msg.format(func_name, 'unsuccessfully'))
+            logger.info(finishing_msg.format(func_name, 'unsuccessfully'))
             raise
         else:
-            self._logger.info(finishing_msg.format(func_name, 'successfully'))
+            logger.info(finishing_msg.format(func_name, 'successfully'))
 
         return result
 
