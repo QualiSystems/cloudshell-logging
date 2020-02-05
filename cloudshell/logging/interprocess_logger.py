3ae1419b470c1d1e967382d196159793d70a825a
Yaroslav Nikonorov <yaroslav.n@quali.com>
2020-02-05 16:54:44 +0200

    Fixed handler daemon, fixed close for multiprocessinbg

--- cloudshell/logging/interprocess_logger.py
+++ cloudshell/logging/interprocess_logger.py
@@ -3,7 +3,12 @@ import multiprocessing
 import sys
 import threading
 import traceback
+from _queue import Empty
 from logging.handlers import RotatingFileHandler
+
+
+class MultiProcessingLogException(Exception):
+    pass
 
 
 class MultiProcessingLog(logging.Handler):
@@ -13,28 +18,36 @@ class MultiProcessingLog(logging.Handler):
         self._handler = RotatingFileHandler(name, mode, maxsize, rotate)
         self.queue = multiprocessing.Queue(-1)
         self.baseFilename = name
-        t = threading.Thread(target=self.receive)
-        t.daemon = True
-        t.start()
+        self._is_closed = False
+        self._receive_thread = threading.Thread(target=self.receive)
+        self._receive_thread.daemon = True
+        self._receive_thread.start()
 
     def setFormatter(self, fmt):
         logging.Handler.setFormatter(self, fmt)
         self._handler.setFormatter(fmt)
 
     def receive(self):
-        while True:
+        while not self._is_closed or not self.queue.empty():
             try:
-                record = self.queue.get()
+                record = self.queue.get(block=True, timeout=0.1)
                 self._handler.emit(record)
+            except Empty:
+                continue
             except (KeyboardInterrupt, SystemExit):
                 raise
             except EOFError:
                 break
             except Exception:
                 traceback.print_exc(file=sys.stderr)
+        self.queue.close()
+        self.queue.join_thread()
 
     def send(self, s):
-        self.queue.put_nowait(s)
+        if not self._is_closed:
+            self.queue.put_nowait(s)
+        else:
+            raise MultiProcessingLogException("Cannot send record, when handler closed")
 
     def _format_record(self, record):
         # ensure that exc_info and args
@@ -60,5 +73,7 @@ class MultiProcessingLog(logging.Handler):
             self.handleError(record)
 
     def close(self):
+        self._is_closed = True
+        self._receive_thread.join(5)
         self._handler.close()
         logging.Handler.close(self)
